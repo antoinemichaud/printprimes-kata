85d6002e0dc5910e037ae8979e890558f60625c2
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-02-24 07:50:32 +0100

    Init PrintPrimes Kata

---
+++ src/test/java/com/dolounet/printprimes/AppTest.java
@@ -0,0 +1,13 @@
+package com.dolounet.printprimes;
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertThat(true).isTrue();
+    }
+}
